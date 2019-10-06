96859785cb5b4f0df15a6bb4974baf43108dd4ee
Jacek Jankowski <jacek143@gmail.com>
2019-10-06 17:10:58 +0200

    Run script from every directory

--- GITuck.sh
+++ GITuck.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+QUACK_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_PATH=$1
 cd ${REPO_PATH}
 RESULT=$(git remote update --prune 2>&1 >/dev/null)
 cd - > /dev/null
 if [[ ${RESULT} ]]; then
   echo ${RESULT}
-  aplay --quiet quack.wav
+  aplay --quiet ${QUACK_PATH}/quack.wav
 fi
